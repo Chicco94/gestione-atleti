daed51a8956ed3a9b500b570bbd1e85ea2b0a637
Enrico Cominato <41648709+Chicco94@users.noreply.github.com>
2022-02-15 17:44:21 +0100

    shared module

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,40 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AthleteComponent } from './components/athlete/athlete.component';
-import { EnumToArrayPipe, FilterPipe } from '../utils/pipes/pipes';
+import { EnumToArrayPipe, FilterPipe, SearchPipe } from './pipes/pipes';
 
-// MATERIAL
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatCardModule} from '@angular/material/card';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatInputModule} from '@angular/material/input';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatListModule} from '@angular/material/list';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatSelectModule} from '@angular/material/select';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {ScrollingModule} from '@angular/cdk/scrolling';
-import { AppRoutingModule } from '../app-routing.module';
 import { BaseModule } from '../base/base.module';
+import { AtletaPreviewComponent } from './components/atleta-preview/atleta-preview.component';
+import { TestPreviewComponent } from './components/test-preview/test-preview.component';
+import { ListaAtletiComponent } from './components/lista-atleti/lista-atleti.component';
+import { ListaTestComponent } from './components/lista-test/lista-test.component';
 
 @NgModule({
   declarations: [
     AthleteComponent,
+    AtletaPreviewComponent,
+    TestPreviewComponent,
+    ListaAtletiComponent,
+    ListaTestComponent,
     EnumToArrayPipe,
-    FilterPipe
+    FilterPipe,
+    SearchPipe
   ],
   imports: [
     CommonModule,
-    AppRoutingModule,
     BaseModule
   ],
   exports:[
-    AthleteComponent
   ]
 })
 export class SharedModule { }
