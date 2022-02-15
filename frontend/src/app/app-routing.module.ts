6b91f159893cbf97f917128935271b9b0b12b955
Enrico Cominato <41648709+Chicco94@users.noreply.github.com>
2022-02-15 17:02:57 +0100

    added routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,12 +5,16 @@ import { GestioneComponent } from './gestione/gestione.component';
 import { HomeComponent } from './base/components/home/home.component';
 import { AllenamentoComponent } from './allenamenti/components/allenamento/allenamento.component';
 import { ListaAllenamentiComponent } from './allenamenti/components/lista-allenamenti/lista-allenamenti.component';
+import { ListaAtletiComponent } from './shared/components/lista-atleti/lista-atleti.component';
+import { ListaTestComponent } from './shared/components/lista-test/lista-test.component';
 
 const routes: Routes = [
   {path: 'home', component:HomeComponent},
   {path: 'allenamenti', component:AllenamentiComponent},
   {path: 'listaAllenamenti', component:ListaAllenamentiComponent},
   {path: 'allenamento', component:AllenamentoComponent},
+  {path: 'listaAtleti/:selectable', component:ListaAtletiComponent},
+  {path: 'listaTest', component:ListaTestComponent},
   {path: 'gestione', component:GestioneComponent},
 	{path: '', redirectTo:'/home', pathMatch: 'full'},
 	{ path: '**', redirectTo:'/home' }
