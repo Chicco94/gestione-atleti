06be95e0039e28375537507665f2baf836061568
Enrico Cominato <41648709+Chicco94@users.noreply.github.com>
2022-02-15 17:44:33 +0100

    create test data

--- frontend/src/app/shared/services/athlete.service.ts
+++ frontend/src/app/shared/services/athlete.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { SexEnum } from 'src/app/base/models/enumerations';
 import { Athlete } from 'src/app/shared/models/athlete.model';
 
 @Injectable({
@@ -8,9 +9,26 @@ export class AthleteService {
 	list:Athlete[] = [];
 	constructor() {
 		Object.setPrototypeOf(this, AthleteService.prototype);
+		let a1 = new Athlete();
+		a1.name = "Enrico";
+		a1.surname = "Cominato";
+		a1.sex = SexEnum.male;
+		a1.date_of_birth = new Date("1994-02-07");
+		let a2 = new Athlete();
+		a2.name = "Eleonora";
+		a2.surname = "Barcaro";
+		a2.sex = SexEnum.female;
+		a2.date_of_birth = new Date("2008-10-10");
+		for (let i=0;i<100;i++){
+			this.list.push(a1);
+			this.list.push(a2);
+		}
 	}
 
-	public getAthlete(id:string):Athlete{
+	public getAllAthletes():Athlete[]{
+		return this.list;
+	}
+	public getAthlete(id:number):Athlete{
 		return this.list.find((athlete:Athlete) => athlete.id == id) || new Athlete();
 	}
 }
