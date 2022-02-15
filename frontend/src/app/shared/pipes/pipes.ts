7982062e4a48353c2ae990cb2613376560ed75a3
Enrico Cominato <41648709+Chicco94@users.noreply.github.com>
2022-02-15 17:42:46 +0100

    moved pipes

--- frontend/src/app/shared/pipes/pipes.ts
+++ frontend/src/app/shared/pipes/pipes.ts
@@ -7,6 +7,19 @@ export class FilterPipe implements PipeTransform {
 	}
 }
 
+
+@Pipe({
+  name: 'search'
+})
+export class SearchPipe implements PipeTransform {
+  public transform(value:any[], keys: string, term: string) {
+    if (!term) return value;
+    return (value || []).filter(item => keys.split(',').some(key => item.hasOwnProperty(key) && new RegExp(term, 'gi').test(item[key])));
+
+  }
+}
+
+
 @Pipe({
 	name: 'enumToArray'
   })
