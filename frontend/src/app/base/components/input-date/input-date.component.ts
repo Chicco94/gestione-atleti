02a109b4978c42a765c0a2dfa718ed83ae54d5e5
Enrico Cominato <41648709+Chicco94@users.noreply.github.com>
2022-02-15 18:08:14 +0100

    input date

---
+++ frontend/src/app/base/components/input-date/input-date.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-input-date',
+  templateUrl: './input-date.component.html',
+  styleUrls: ['./input-date.component.scss']
+})
+export class InputDateComponent implements OnInit {
+  @Input() placeholder:string = "";
+  @Input() model:Date = new Date();
+  @Output() modelChange:EventEmitter<Date> = new EventEmitter<Date>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onModelChange(value: Date) {
+    this.modelChange.emit(value);
+  }
+}
