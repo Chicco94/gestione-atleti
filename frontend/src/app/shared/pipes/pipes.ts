f39cf8835c856a999bdf663cba79200adfba2a6f
Enrico Cominato <41648709+Chicco94@users.noreply.github.com>
2022-02-23 15:39:00 +0100

    added comment

--- frontend/src/app/shared/pipes/pipes.ts
+++ frontend/src/app/shared/pipes/pipes.ts
@@ -12,6 +12,13 @@ export class FilterPipe implements PipeTransform {
   name: 'search'
 })
 export class SearchPipe implements PipeTransform {
+  /**
+   * @description Data una lista di elementi omogenei, degli attributi degli elementi ed una stringa da cercare ritorna tutti gli elementi della lista passata in cui l'elemento da cercare compare tra gli attributi indicati
+   * @param value lista di elementi omogenei
+   * @param keys nomi degli attributi tra cui cercare divisi da ","
+   * @param term stringa da ricercare
+   * @returns lista degli elementi in cui compare la stringa cercata negli attributi indicati
+   */
   public transform(value:any[], keys: string, term: string) {
     if (!term) return value;
     return (value || []).filter(item => keys.split(',').some(key => item.hasOwnProperty(key) && new RegExp(term, 'gi').test(item[key])));
