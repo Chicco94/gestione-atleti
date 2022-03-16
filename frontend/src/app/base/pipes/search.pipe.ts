71a79e38e7bdc9f974b4313bf0187f5e85db2034
Enrico Cominato <41648709+Chicco94@users.noreply.github.com>
2022-03-16 18:24:05 +0100

    fixed error in search

--- frontend/src/app/base/pipes/search.pipe.ts
+++ frontend/src/app/base/pipes/search.pipe.ts
@@ -13,7 +13,7 @@ export class SearchPipe implements PipeTransform {
    */
   public transform(value:any[], keys: string, term: string) {
     if (!term) return value;
-    return (value || []).filter(item => keys.split(',').some(key => item.hasOwnProperty(key) && new RegExp(term, 'gi').test(item[key])));
+    return (value || []).filter(item => keys.split(',').some(key => item.hasOwnProperty(key) && String(item[key]).toLowerCase().includes(term.toLowerCase())));
 
   }
 }
