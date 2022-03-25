7e076f4735bf815c044386c389fd53c8890235cd
Enrico Cominato <41648709+Chicco94@users.noreply.github.com>
2022-03-25 17:47:49 +0100

    change order

--- frontend/src/app/allenamenti/allenamenti-routing.module.ts
+++ frontend/src/app/allenamenti/allenamenti-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AllenamentiComponent } from './allenamenti.component';
-import { AllenamentiLandingComponent } from './components/allenamenti-landing/allenamenti-landing.component';
-import { AllenamentoAttivoComponent } from './components/allenamento-attivo/allenamento-attivo.component';
-import { AllenamentoLandingComponent } from './components/allenamento-landing/allenamento-landing.component';
-import { AllenamentoSelectAtletaComponent } from './components/allenamento-select-atleta/allenamento-select-atleta.component';
-import { AllenamentoSelectTestComponent } from './components/allenamento-select-test/allenamento-select-test.component';
 import { AllenamentoComponent } from './components/allenamento/allenamento.component';
-import { ListaAllenamentiLandingComponent } from './components/lista-allenamenti-landing/lista-allenamenti-landing.component';
 import { ListaAllenamentiComponent } from './components/lista-allenamenti/lista-allenamenti.component';
+import { AllenamentoAttivoComponent } from './components/allenamento-attivo/allenamento-attivo.component';
+import { AllenamentiLandingComponent } from './components/allenamenti-landing/allenamenti-landing.component';
+import { AllenamentoLandingComponent } from './components/allenamento-landing/allenamento-landing.component';
+import { AllenamentoSelectTestComponent } from './components/allenamento-select-test/allenamento-select-test.component';
+import { AllenamentoSelectAtletaComponent } from './components/allenamento-select-atleta/allenamento-select-atleta.component';
+import { ListaAllenamentiLandingComponent } from './components/lista-allenamenti-landing/lista-allenamenti-landing.component';
 
 const allenamentiRoutes: Routes = [
 	{path: 'allena', component:AllenamentiLandingComponent,
