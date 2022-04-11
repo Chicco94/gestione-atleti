65471f5aedb9ef0ac45e3cf3d9dfb6f2103876c6
Enrico Cominato <41648709+Chicco94@users.noreply.github.com>
2022-04-11 16:43:44 +0200

    login test

--- frontend/src/app/shared/guards/login.guard.spec.ts
+++ frontend/src/app/shared/guards/login.guard.spec.ts
@@ -1,14 +1,20 @@
+import { ActivatedRouteSnapshot, Router } from '@angular/router';
+
 import { TestBed } from '@angular/core/testing';
 
 import { LoginGuard } from './login.guard';
+import { AuthenticationService } from '../services/authentication.service';
 
 describe('LoginGuard', () => {
 	let guard: LoginGuard;
+	let routerSpy: jasmine.SpyObj<Router>;
+	let serviceStub: Partial<AuthenticationService>;
 
 	beforeEach(() => {
-		TestBed.configureTestingModule({});
-		guard = TestBed.inject(LoginGuard);
-	});
+		routerSpy = jasmine.createSpyObj<Router>('Router', ['navigate']); // [1]
+		serviceStub = {}; // [2]
+		guard = new LoginGuard(routerSpy,serviceStub as AuthenticationService); // [3]
+	  });
 
 	it('should be created', () => {
 		expect(guard).toBeTruthy();
